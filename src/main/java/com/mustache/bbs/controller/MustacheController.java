5337e10088f7a2ffbd37f514d07f7f21ef79b814
psi5059 <psi505968@gmail.com>
2022-11-07 13:50:54 +0900

    GetMapping 추가

--- src/main/java/com/mustache/bbs/controller/MustacheController.java
+++ src/main/java/com/mustache/bbs/controller/MustacheController.java
@@ -9,15 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class MustacheController {
     @GetMapping(value = "/hi")
     public String mustacheCon(Model model) {
-        model.addAttribute("username", "rok");// view에 값을 넘기겠다.
+        model.addAttribute("username", "kny");// view에 값을 넘기겠다.
         return "greetings"; // greetings라는 이름의 view를 리턴
     }
 
     //PathVariable로부터 넘어온 값 넘기기
     @GetMapping(value = "/hi/{id}")
     public String mustacheCon2(@PathVariable String id, Model model) {
-        model.addAttribute("username", "rok");// view에 값을 넘기겠
-        model.addAttribute("id", id);// view에 값을 넘기겠
+        model.addAttribute("username", "kny");// view에 값을 넘기겠
+        model.addAttribute("id", id);// view에 값을 넘기겠다
         return "greetings"; // greetings라는 이름의의 view를 리턴
     }
 
