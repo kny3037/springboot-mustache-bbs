7b2373fbe4ad2b4449534541e74d39cd322a996b
psi5059 <psi505968@gmail.com>
2022-11-09 14:44:36 +0900

    컬럼명 오류 수정

--- src/main/java/com/mustache/bbs/domain/entity/Article.java
+++ src/main/java/com/mustache/bbs/domain/entity/Article.java
@@ -1,31 +1,33 @@
 package com.mustache.bbs.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-// @Entity, @Id, @GeneratedValue 세 개는 세트!
-
-@Entity
-//JPA에서 객체로 다루겠다는 선언
-@NoArgsConstructor
 @Getter
+@Entity //JPA에서 객체로 다루겠다는 선언
+@NoArgsConstructor
+@AllArgsConstructor
 public class Article {
     @Id
     //@Entity가 붙어있다면 꼭 붙여줘야 한다. pk를 의미.
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     //자동 완성
     //ID를 직접 생성하지 않고 자동으로 생성하도록 한 경우 붙인다.
     private Long id;
 
     private String title;
-    private String contents;
+    private String content;
 
-    public Article(String title, String contents) {
+    public Article(String title, String content) {
         this.title = title;
-        this.contents = contents;
+        this.content = content;
     }
 }
+
+// @Entity, @Id, @GeneratedValue 세 개는 세트!
